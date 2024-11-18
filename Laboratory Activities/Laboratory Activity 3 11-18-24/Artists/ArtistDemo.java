e3301737207211c6ed332d1387d06d02f1469b15
Richnell <165745941+Richnell@users.noreply.github.com>
2024-11-18 10:16:50 +0800

    Update ArtistDemo.java

--- Laboratory Activities/Laboratory Activity 3 11-18-24/Artists/ArtistDemo.java
+++ Laboratory Activities/Laboratory Activity 3 11-18-24/Artists/ArtistDemo.java
@@ -1,22 +1,22 @@
 public class ArtistDemo {
     public static void main(String[] args) {
-        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        Artist artist = new Artist(" Taylor Swift", "American", 34, "Music");
         artist.displayInfo();
         System.out.println();
-
-        Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
+        
+        Singer singer = new Singer("Elvis Presley", "American", 42, "Music", Genre.CLASSICAL);
         singer.displayInfo();
         System.out.println();
-
-        Painter painter = new Painter("Vincent van Gogh", "Dutch", 37, "Painting", Medium.OIL);
+        
+        Painter painter = new Painter("Claude Monet", "French", 86, "Painting", Medium.WATERCOLOR);
         painter.displayInfo();
         System.out.println();
-
-        Writer writer = new Writer("J.K. Rowling", "British", 58, "Writing", WritingStyle.FICTION);
+        
+        Writer writer = new Writer("Jane Austen", "British", 41, "Writing", WritingStyle.FICTION);
         writer.displayInfo();
         System.out.println();
-
-        Dancer dancer = new Dancer("Mikhail Baryshnikov", "Russian", 76, "Dancing", DanceStyle.BALLET);
+        
+        Dancer dancer = new Dancer("Anna Pavlova", "Russian", 49, "Dancing", DanceStyle.BALLET);
         dancer.displayInfo();
     }
 }
